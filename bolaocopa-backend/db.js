4f85883fa92f32c9b0b7543125a67f3e78aaab31
TheLuizSouza <g7luiz@gmail.com>
2026-06-11 14:26:35 -0300

    Deployando primeira versão com SQL em cloud

--- bolaocopa-backend/db.js
+++ bolaocopa-backend/db.js
@@ -1,13 +1,14 @@
 // db.js
+import dotenv from "dotenv";
+dotenv.config();
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: 
+    process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false  
 });
 
 module.exports = pool;
