7d14c48327e6a3626c7715821c40165eacb53149
TheLuizSouza <g7luiz@gmail.com>
2026-06-11 18:47:13 -0300

    SQL em nuvem

--- bolaocopa-backend/db.js
+++ bolaocopa-backend/db.js
@@ -1,14 +1,10 @@
 // db.js
-import dotenv from "dotenv";
-dotenv.config();
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: 
-    process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false  
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false  
 });
 
 module.exports = pool;
